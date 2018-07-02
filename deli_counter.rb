d8213c505d8f0161e1c5c3e471bbb4fc0600510e
Kristen Papke <klpapke215@gmail.com>
2018-07-02 01:29:12 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -13,7 +13,8 @@ end
 
 
 def take_a_number(katz_deli, name)
-  puts "Welcome, #{name}. You are number [index + 1] in line."
+  katz_deli << name
+  puts "Welcome, #{name}. You are number #{katz_deli.length + 1} in line."
 end
 
 def now_serving(katz_deli)
