94f06a2a81b13e1aa5b91e784874800ee6059be6
Kristen Papke <klpapke215@gmail.com>
2018-07-02 00:53:24 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,6 +4,9 @@ def line(katz_deli)
     puts "The line is currently empty."
   elsif katz_deli.length > 0
     line = ["The line is currently:"]
+    katz_deli.each do |name|
+      line << name 
+    end
     puts line.join
   end
 end
