2a5a3639cc4ca0eeafdf5e5a0096be46de9e5808
Kristen Papke <klpapke215@gmail.com>
2018-07-02 00:59:11 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,7 +5,7 @@ def line(katz_deli)
   elsif katz_deli.length > 0
     line = ["The line is currently:"]
     katz_deli.each_with_index do |name, index|
-      line << "index. name"
+      line << "index. #{name}"
     end
     puts line.join
   end
